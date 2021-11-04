e8f1e250b925c41cb76d48bc840b7ee63b6ded45
Nedine <nedinerafid@gmail.com>
2021-11-04 17:16:13 +0200

    Initial Commit

---
+++ swiftui kill me try2/swiftui_kill_me_try2App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_kill_me_try2App.swift
+//  swiftui kill me try2
+//
+//  Created by Yasmine Rafid on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_kill_me_try2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
